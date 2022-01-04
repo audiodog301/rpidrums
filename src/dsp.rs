6e72df25bca96c4ae58195b6d17c3e2249e96415
Liam Fissell <liam@Liams-MacBook-Pro.local>
2022-01-04 12:40:20 -0600

    sine wave plays

--- src/dsp.rs
+++ src/dsp.rs
@@ -1,1 +1,21 @@
-pub struct Sine;
+pub struct Sine {
+    phase: f32,
+    freq: f32,
+    sample_rate: f32,
+}
+
+impl Sine {
+    pub fn new(freq: f32, sample_rate: f32) -> Self {
+        Self {
+            phase: 0.0,
+            freq,
+            sample_rate,
+        }
+    }
+
+    pub fn process(&mut self) -> f32 { //lol credit to WeirdConstructor for this code i hate writing oscillators
+        let output = (self.phase * std::f32::consts::TAU).sin();
+        self.phase = (self.phase + self.freq / self.sample_rate).fract();
+        output
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,37 @@
-use cpal::{Data, Sample, SampleFormat};
+use cpal::{Sample};
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 
+mod dsp;
 use dsp::Sine;
 
 fn main() {
-    let err_fn = |err| eprintln!("an error occurred on the output audio stream: {}", err);
+    let host = cpal::default_host();
+    let device = host.default_output_device().expect("no output device available");
+
+    let mut supported_configs_range = device.supported_output_configs()
+        .expect("error while querying configs");
+    let supported_config = supported_configs_range.next()
+        .expect("no supported config?!")
+        .with_max_sample_rate();
+    
     let sample_format = supported_config.sample_format();
     let config = supported_config.into();
-    let stream = match sample_format {
-        SampleFormat::F32 => device.build_output_stream(&config, write_silence::<f32>, err_fn),
-        SampleFormat::I16 => device.build_output_stream(&config, write_silence::<i16>, err_fn),
-        SampleFormat::U16 => device.build_output_stream(&config, write_silence::<u16>, err_fn),
-    }.unwrap();
-}
 
-fn write_silence<T: Sample>(data: &mut [T], _: &cpal::OutputCallbackInfo) {
-    for sample in data.iter_mut() {
-        *sample = Sample::from(&0.0);
-    }
+    let mut sine = Sine::new(110.0, 44_100.0);
+    
+    let stream = device.build_output_stream(
+        &config,
+        move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+            for sample in data.iter_mut() {
+                *sample = Sample::from(&sine.process());
+            }
+        },
+        move |err| {
+            // react to errors here.
+        },
+    ).unwrap();
+
+    stream.play().unwrap();
+    
+    loop {}
 }

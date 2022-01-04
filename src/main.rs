eda3acc2a911ac86baff915574ff32f32887cc8a
Liam Fissell <liam@Liams-MacBook-Pro.local>
2022-01-04 11:07:59 -0600

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use cpal::{Data, Sample, SampleFormat};
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+
+use dsp::Sine;
+
+fn main() {
+    let err_fn = |err| eprintln!("an error occurred on the output audio stream: {}", err);
+    let sample_format = supported_config.sample_format();
+    let config = supported_config.into();
+    let stream = match sample_format {
+        SampleFormat::F32 => device.build_output_stream(&config, write_silence::<f32>, err_fn),
+        SampleFormat::I16 => device.build_output_stream(&config, write_silence::<i16>, err_fn),
+        SampleFormat::U16 => device.build_output_stream(&config, write_silence::<u16>, err_fn),
+    }.unwrap();
+}
+
+fn write_silence<T: Sample>(data: &mut [T], _: &cpal::OutputCallbackInfo) {
+    for sample in data.iter_mut() {
+        *sample = Sample::from(&0.0);
+    }
+}
